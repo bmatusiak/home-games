73450cac7bd3afcb54db9719b5ad3c6c198cc4c4
Bradley Matusiak <bmatusiak@gmail.com>
2025-01-02 22:15:24 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), 
+    filename: '[name].bundle.js', 
     publicPath: '/home-games/', 
+    clean: true, 
   },
   module: {
     rules: [
