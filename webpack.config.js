ed5dffa694c6283778a2fac52a6a011a95e04c17
Bradley Matusiak <bmatusiak@gmail.com>
2025-01-02 22:01:16 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/your-project-name/', 
+    publicPath: '/home-games/', 
   },
   module: {
     rules: [
